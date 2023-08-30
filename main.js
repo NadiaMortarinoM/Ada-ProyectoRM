32816d1067082254e7037d45326feebb5697dca2
NadiaMortarinoM <nmortarinom@gmail.com>
2023-08-29 22:09:03 -0300

    botones

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 //primero declaramos la variable que contiene todas las tarjetas:
 const container = document.getElementById("container");
-console.log(container);
+let currentPage = 1;
+let totalPages=0;
+//filtros (género: tener botones de todos, mujeres, hombres, desconocido)
+//radious button
+const todosBtn = document.getElementById("todos");
+const mujeresBtn = document.getElementById("mujeres");
+const hombresBtn = document.getElementById("hombres");
+const desconocidoBtn = document.getElementById("desconocido");
 
-const getCharacters = () => {
-    fetch ("https://rickandmortyapi.com/api/character")
+
+const getCharacters = (pageNumber) => {
+    container.innerHTML="";
+    fetch (`https://rickandmortyapi.com/api/character?page=${pageNumber}`)
     .then ((res) => res.json())
-    .then ((data) => imageOfCharacters (data))
+    .then ((data) => {
+        imageOfCharacters(data)
+totalPages = data.info.pages;
+    })
 }
 
-const imageOfCharacters = (data) => {
-    console.log(data);
 
+const imageOfCharacters = (data) => {
 data.results.forEach(character => {
 container.innerHTML +=
 `<div class="card">
@@ -45,4 +56,52 @@ const BackToHome = () => {
     location.reload();
 }
 
-getCharacters();
+getCharacters(currentPage);
+
+
+
+// Evento para avanzar a la siguiente página
+const nextBtn = document.getElementById("nextButton");
+const prevBtn =document.getElementById("prevButton");
+
+nextBtn.addEventListener("click", () => {
+if (currentPage <= 1)   {
+    currentPage++;
+} else if (currentPage >1 && currentPage < totalPages) { //pasamos de la página 2 a la 41
+    prevBtn.removeAttribute("disabled", false)
+    currentPage++;
+} else  {
+    nextBtn.setAttribute("disabled", true)
+} 
+
+getCharacters(currentPage);
+   
+});
+// Evento para la página anterior:
+prevBtn.addEventListener("click", () => {
+    if (currentPage <= 1)    {
+        prevBtn.setAttribute("disabled", true)
+    } else if (currentPage >1 && currentPage <= totalPages) { 
+        currentPage--;
+        nextBtn.removeAttribute("disabled", true)    
+    } else  {
+        nextBtn.setAttribute("disabled", true)
+        currentPage--;
+    } 
+    
+    getCharacters(currentPage);
+       
+    });
+//const filterCharacters = (filterParam, valueParam) =>{
+    //fetch(`https://rickandmortyapi.com/api/character/?${filterParam}=${valueParam}`).then(res=>res.json()).then(data=>imageOfCharacters(data))
+//}
+//filtros
+//mujeresBtn.addEventListener("click", ()=>
+    //filterCharacters("gender", "female"));
+
+
+
+
+
+
+
